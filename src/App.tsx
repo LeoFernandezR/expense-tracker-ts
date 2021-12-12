ff7ea2538d9c09d421412c3dc7bdfb7e0de7cd46
Leonardo Fernández <leonardo.fernandez@gmail.com>
2021-12-11 22:34:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import ExpenseItem from "./components/ExpenseItem"
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h2>Hi Mom</h2>
+      <ExpenseItem />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ExpenseItem: React.FC = () => {
+  return (
+    <div>
+      <div>Data</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
